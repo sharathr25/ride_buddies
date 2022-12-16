6f270a6fb8d2fc9e5d2b41955c97684379c8af35
sharathr25 <sharathdupati@gmail.com>
2022-12-16 16:15:44 +0530

    added OTP screen

---
+++ src/components/molecules/PinInput/PinInput.js
@@ -0,0 +1,74 @@
+import React, { useContext, useRef } from 'react';
+import { Pressable, TextInput as RNTextInput } from 'react-native';
+import { ThemeContext } from '../../../ThemeContext';
+import Box from '../../atoms/Box';
+import Text from '../../atoms/Text';
+import Error from '../Error';
+
+const PinInput = ({ placeholder = '', label, error, maxPin, value, ...rest }) => {
+  const inputRef = useRef();
+  const theme = useContext(ThemeContext);
+
+  const handleOnPress = () => {
+    inputRef.current.focus();
+  };
+
+  const boxDigit = (_, i) => {
+    return (
+      <Box
+        style={{
+          flex: 1 / maxPin,
+          height: 45,
+          borderRadius: 5,
+          marginRight: i === maxPin - 1 ? 0 : 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        backgroundColor="background"
+        key={i}
+      >
+        <Text variant="subHeader">{value[i] || ''}</Text>
+      </Box>
+    );
+  };
+
+  return (
+    <Box>
+      <Box
+        padding="s"
+        style={{
+          position: 'relative',
+          alignSelf: 'stretch',
+          borderWidth: 2,
+          borderRadius: 5,
+          borderColor: theme.colors.lightGray,
+          backgroundColor: `${theme.colors.foreground}50`,
+          color: theme.colors.foreground,
+        }}
+      >
+        {label && (
+          <Text
+            color="foreground"
+            style={{ fontWeight: '600', textTransform: 'capitalize', opacity: 0.5 }}
+          >
+            {label}
+          </Text>
+        )}
+        <Pressable style={{ width: '100%', flexDirection: 'row' }} onPress={handleOnPress}>
+          {new Array(maxPin).fill(0).map(boxDigit)}
+        </Pressable>
+        <RNTextInput
+          value={value}
+          maxLength={maxPin}
+          style={{ color: theme.colors.foreground, padding: 0, height: 0 }}
+          ref={inputRef}
+          keyboardType="number-pad"
+          {...rest}
+        />
+      </Box>
+      <Error error={error} />
+    </Box>
+  );
+};
+
+export default PinInput;
---
+++ src/screens/OTP/OTP.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-native';
+import Box from '../../components/atoms/Box';
+import Button from '../../components/molecules/Button';
+import Text from '../../components/atoms/Text';
+import PinInput from '../../components/molecules/PinInput';
+import useForm from '../../hooks/useForm';
+
+import SVGImg from '../../images/illustrations/letter.svg';
+
+const MAX_PIN = 6;
+
+const OTP = () => {
+  const onValidate = ({ otp }) => {
+    const errors = {};
+    if (otp.length !== MAX_PIN) errors.otp = 'Invalid OTP';
+    return errors;
+  };
+
+  const onSubmit = (values) => {
+    // TODO: login with mobile number
+    console.log(values);
+  };
+
+  const { form, setForm, isValid, validate, handleSubmit } = useForm({
+    initialValues: { otp: '' },
+    onValidate,
+    onSubmit,
+  });
+
+  return (
+    <Box backgroundColor="background" padding="xl" style={{ flex: 1, justifyContent: 'flex-end' }}>
+      <Box style={{ flex: 1 }}>
+        <SVGImg width="100%" height="100%" />
+      </Box>
+      <Text variant="header">OTP?</Text>
+      <Text>We have sent an OTP to xxxxx-xx999</Text>
+      <Box margin="m" />
+      <PinInput
+        label="One time password"
+        value={form.values.otp}
+        onChangeText={setForm('otp')}
+        onBlur={validate}
+        error={form.errors.otp}
+        maxPin={MAX_PIN}
+      />
+      <Box margin="s" />
+      <Button title="Submit" onPress={handleSubmit} disabled={!isValid()} />
+      <Box margin="m" />
+      <Box style={{ flexDirection: 'row' }}>
+        <Text>Wrong number? </Text>
+        <Link to="/sign-up">
+          <Text color="link">Go back</Text>
+        </Link>
+      </Box>
+    </Box>
+  );
+};
+
+export default OTP;
