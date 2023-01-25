571c5f4cdc400c427324f96c5da028a195d6cff1
sharathr25 <sharathdupati@gmail.com>
2023-01-25 18:01:02 +0530

    env check

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,9 +1,9 @@
 export const API_URL =
-  process.env.NODE_ENV !== 'development'
+  process.env.NODE_ENV === 'development'
     ? 'http://192.168.29.232:3000'
     : 'https://rider-buddies-server.onrender.com';
 
 export const SOCKET_URL =
-  process.env.NODE_ENV !== 'development'
+  process.env.NODE_ENV === 'development'
     ? 'ws://192.168.29.232:3000'
     : 'wss://rider-buddies-server.onrender.com';
